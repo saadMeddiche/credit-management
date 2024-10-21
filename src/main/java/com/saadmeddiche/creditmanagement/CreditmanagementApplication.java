2e5a2f599c446b9ebddf915ebed0895342cace53
SaadMeddiche <s.meddiche@itroadgroup.com>
2024-10-21 16:30:40 +0100

    generate project

---
+++ src/main/java/com/saadmeddiche/creditmanagement/CreditmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.saadmeddiche.creditmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadmeddiche/creditmanagement/CreditmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadmeddiche.creditmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
