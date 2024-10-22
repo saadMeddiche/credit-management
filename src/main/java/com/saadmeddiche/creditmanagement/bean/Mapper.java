9979ba86999f6a8d61e7ecff24d9291162990840
SaadMeddiche <s.meddiche@itroadgroup.com>
2024-10-22 10:17:51 +0100

    add model mapper

---
+++ src/main/java/com/saadmeddiche/creditmanagement/bean/Mapper.java
@@ -0,0 +1,16 @@
+package com.saadmeddiche.creditmanagement.bean;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class Mapper {
+
+    @Bean @Scope("prototype")
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
