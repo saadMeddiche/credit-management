7b5e73c079adc1ae3f16c210134038e16de5c746
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-01 09:12:32 +0100

    EnableCaching

--- src/main/java/com/saadmeddiche/creditmanagement/CreditManagementApplication.java
+++ src/main/java/com/saadmeddiche/creditmanagement/CreditManagementApplication.java
@@ -2,7 +2,9 @@ package com.saadmeddiche.creditmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CreditManagementApplication {
 
