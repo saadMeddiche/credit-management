887442c33add5043e97a4c11753696db1dc5a57b
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-01 15:01:46 +0100

    create PersonAPIs constants

---
+++ src/main/java/com/saadmeddiche/creditmanagement/global_constants/PersonAPIs.java
@@ -0,0 +1,9 @@
+package com.saadmeddiche.creditmanagement.global_constants;
+
+public class PersonAPIs {
+    public static final String PERSONS = "/persons";
+    public static final String PERSONS_GET = "/person/{id}";
+    public static final String PERSONS_UPDATE = "/person/{id}";
+    public static final String PERSONS_CREATE = "/persons";
+    public static final String PERSONS_DELETE = "/persons/{id}";
+}
