c2ef8d07149258e4fa29a69505ebefa21997b9da
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-04 09:48:46 +0100

    rename endpoints related to person

--- src/main/java/com/saadmeddiche/creditmanagement/global_constants/PersonAPIs.java
+++ src/main/java/com/saadmeddiche/creditmanagement/global_constants/PersonAPIs.java
@@ -2,8 +2,8 @@ package com.saadmeddiche.creditmanagement.global_constants;
 
 public class PersonAPIs {
     public static final String PERSONS = "/persons";
-    public static final String PERSONS_GET = "/person/{id}";
-    public static final String PERSONS_UPDATE = "/person/{id}";
+    public static final String PERSONS_GET = "/persons/{id}";
+    public static final String PERSONS_UPDATE = "/persons/{id}";
     public static final String PERSONS_CREATE = "/persons";
     public static final String PERSONS_DELETE = "/persons/{id}";
 }
