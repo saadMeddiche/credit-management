0e2d360d3b2e7ed07fb5fa10c64f14099cf165d7
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-12 16:34:31 +0100

    create CreditCreateRequest

---
+++ src/main/java/com/saadmeddiche/creditmanagement/dtos/CreditCreateRequest.java
@@ -0,0 +1,21 @@
+package com.saadmeddiche.creditmanagement.dtos;
+
+import com.saadmeddiche.creditmanagement.annotations.DateComparison;
+import com.saadmeddiche.creditmanagement.enums.CreditType;
+import com.saadmeddiche.creditmanagement.enums.Currency;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDateTime;
+
+@DateComparison(startField = "grantDate", endField = "refundDate", message = "Refund date must be after grant date")
+public record CreditCreateRequest(
+        @NotNull @Positive Double amount,
+        @NotNull Currency currency,
+        @NotNull CreditType creditType,
+        @NotBlank @NotEmpty String reason,
+        @NotNull LocalDateTime grantDate,
+        LocalDateTime refundDate
+) { }
