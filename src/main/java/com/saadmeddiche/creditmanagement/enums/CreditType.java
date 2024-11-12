0e5f5c7a10225a29abe73f793f9d3e1c58eacbfa
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-12 16:34:20 +0100

    create CreditType enum

---
+++ src/main/java/com/saadmeddiche/creditmanagement/enums/CreditType.java
@@ -0,0 +1,6 @@
+package com.saadmeddiche.creditmanagement.enums;
+
+public enum CreditType {
+    ON_ME, // mean the user need to pay the credit to person who grant it
+    ON_OTHER // mean the user need to receive the credit from person who grant it
+}
