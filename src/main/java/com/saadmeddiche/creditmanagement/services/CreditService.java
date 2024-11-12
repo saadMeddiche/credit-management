74052803bb5dfd634a4463ceb90f5ab280aec4f8
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-12 16:34:57 +0100

    create CreditService.createCredit()

--- src/main/java/com/saadmeddiche/creditmanagement/services/CreditService.java
+++ src/main/java/com/saadmeddiche/creditmanagement/services/CreditService.java
@@ -1,6 +1,12 @@
 package com.saadmeddiche.creditmanagement.services;
 
+import com.saadmeddiche.creditmanagement.dtos.CreditCreateRequest;
+import com.saadmeddiche.creditmanagement.entities.Credit;
+import com.saadmeddiche.creditmanagement.entities.Person;
+import com.saadmeddiche.creditmanagement.repositories.CreditRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -8,4 +14,18 @@ import org.springframework.validation.annotation.Validated;
 @Service @Transactional
 @RequiredArgsConstructor @Validated
 public class CreditService {
+
+    private final CreditRepository creditRepository;
+
+    private final ModelMapper modelMapper;
+
+    public void createCredit(Person person, @Valid CreditCreateRequest creditCreateRequest){
+
+        Credit credit = modelMapper.map(creditCreateRequest, Credit.class);
+
+        credit.setPerson(person);
+
+        creditRepository.save(credit);
+
+    }
 }
