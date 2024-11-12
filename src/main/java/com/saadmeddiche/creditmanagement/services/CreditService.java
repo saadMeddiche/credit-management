1055e359d07ef07ea67bc66167ac79ed889c13e9
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-12 13:53:50 +0100

    create CreditService

---
+++ src/main/java/com/saadmeddiche/creditmanagement/services/CreditService.java
@@ -0,0 +1,11 @@
+package com.saadmeddiche.creditmanagement.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+@Service @Transactional
+@RequiredArgsConstructor @Validated
+public class CreditService {
+}
