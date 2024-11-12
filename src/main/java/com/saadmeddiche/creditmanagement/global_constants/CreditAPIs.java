cdc63eca6376cae071bd652629b7c269c5be0dc2
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-12 16:48:33 +0100

    create CreditAPIs

---
+++ src/main/java/com/saadmeddiche/creditmanagement/global_constants/CreditAPIs.java
@@ -0,0 +1,5 @@
+package com.saadmeddiche.creditmanagement.global_constants;
+
+public class CreditAPIs {
+    public static final String CREATE_CREDIT = "/credits/{personId}"; // personId is the person id to whom the credit is created
+}
