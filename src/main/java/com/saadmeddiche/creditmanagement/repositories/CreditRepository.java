defeefcabf62470c280e97825975ed2e2eb40655
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-19 15:38:38 +0100

    modify findCreditsThatReachedTheirPaymentDate

--- src/main/java/com/saadmeddiche/creditmanagement/repositories/CreditRepository.java
+++ src/main/java/com/saadmeddiche/creditmanagement/repositories/CreditRepository.java
@@ -1,6 +1,7 @@
 package com.saadmeddiche.creditmanagement.repositories;
 
 import com.saadmeddiche.creditmanagement.entities.Credit;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface CreditRepository extends JpaRepository<Credit, Long> {
 
-    @Query("SELECT c FROM Credit c join fetch c.person p join fetch p.phoneNumbers WHERE c.paymentDate <= CURRENT_TIMESTAMP")
+    @EntityGraph(value = "Credit.withPerson")
+    @Query("SELECT c FROM Credit c WHERE c.paymentDate <= CURRENT_TIMESTAMP")
     List<Credit> findCreditsThatReachedTheirPaymentDate();
 }
