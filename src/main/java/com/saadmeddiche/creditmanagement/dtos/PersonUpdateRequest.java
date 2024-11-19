f1ee3f7465c13641b1df170d01c9500154f55d86
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-19 16:41:04 +0100

    remove phoneNumberRequests from PersonUpdateRequest

--- src/main/java/com/saadmeddiche/creditmanagement/dtos/PersonUpdateRequest.java
+++ src/main/java/com/saadmeddiche/creditmanagement/dtos/PersonUpdateRequest.java
@@ -2,20 +2,16 @@ package com.saadmeddiche.creditmanagement.dtos;
 
 import com.saadmeddiche.creditmanagement.annotations.NotExist;
 import com.saadmeddiche.creditmanagement.entities.Person;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
-import java.util.List;
 
 public record PersonUpdateRequest(
         @NotBlank @NotEmpty String firstName,
         @NotBlank @NotEmpty String lastName,
         @NotBlank @NotEmpty @Email @NotExist(entity = Person.class , fieldName = "email") String email,
         @NotBlank @NotEmpty String job,
-        String description,
-        @Valid @NotNull List<PhoneNumberRequest> phoneNumberRequests
+        String description
 ) {
 }
