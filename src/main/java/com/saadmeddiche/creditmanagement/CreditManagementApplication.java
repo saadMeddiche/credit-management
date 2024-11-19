374038421a69ba38322d174349281845ee830ded
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-19 14:58:41 +0100

    @EnableScheduling

--- src/main/java/com/saadmeddiche/creditmanagement/CreditManagementApplication.java
+++ src/main/java/com/saadmeddiche/creditmanagement/CreditManagementApplication.java
@@ -3,8 +3,9 @@ package com.saadmeddiche.creditmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
+@EnableCaching @EnableScheduling
 @SpringBootApplication
 public class CreditManagementApplication {
 
