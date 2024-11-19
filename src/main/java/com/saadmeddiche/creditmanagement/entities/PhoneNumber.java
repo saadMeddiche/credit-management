aba74f7d54c64d5c804f72990acd9eb0a1bb18bc
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-19 14:55:59 +0100

    transform PhoneNumber to entity instead of embeddable

---
+++ src/main/java/com/saadmeddiche/creditmanagement/entities/PhoneNumber.java
@@ -0,0 +1,30 @@
+package com.saadmeddiche.creditmanagement.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Builder @ToString(exclude = "person")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"number", "country_code"}))
+public class PhoneNumber implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_number_seq")
+    private Long id;
+
+    @Column(length = 50)
+    private String number;
+
+    @Column(length = 10)
+    private String countryCode;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("phoneNumbers")
+    private Person person;
+
+}
