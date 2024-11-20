4798da5c8fc816e3e2b2c44c78ab9dfdf4c45912
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-20 14:06:44 +0100

    add handler for HandlerMethodValidationException

--- src/main/java/com/saadmeddiche/creditmanagement/exeptions/ExceptionsRestHandler.java
+++ src/main/java/com/saadmeddiche/creditmanagement/exeptions/ExceptionsRestHandler.java
@@ -5,12 +5,15 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ExceptionsHandler {
+public class ExceptionsRestHandler {
 
+    // This method is used to handle validation errors that are thrown by the @Valid annotation
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) throws MethodArgumentNotValidException {
         return ResponseEntity.badRequest().body(ErrorResponse.builder()
@@ -20,6 +23,19 @@ public class ExceptionsHandler {
                 .timestamp(LocalDateTime.now())
                 .errors(exception.getBindingResult().getFieldErrors().stream().collect(
                         java.util.stream.Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)))
+                .build());
+    }
+
+    // This method is used to handle validation errors that are thrown by the @Valid annotation and the target object is a list
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(HandlerMethodValidationException exception) throws MethodArgumentNotValidException {
+        return ResponseEntity.badRequest().body(ErrorResponse.builder()
+                .status(400)
+                .error("Bad Request")
+                .message("Validation error")
+                .timestamp(LocalDateTime.now())
+                .errors(exception.getAllErrors().stream().map(e -> (FieldError) e).collect(
+                        Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)))
                 .build());
     }
 }
