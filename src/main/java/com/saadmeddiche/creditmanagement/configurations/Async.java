787d0dc6bebc69d91e18bb854fa40616e677ef37
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-11-25 14:44:19 +0100

    Async config

---
+++ src/main/java/com/saadmeddiche/creditmanagement/configurations/Async.java
@@ -0,0 +1,9 @@
+package com.saadmeddiche.creditmanagement.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class Async {
+}
