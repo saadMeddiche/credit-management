a896d5769738b6830c6d373b7cd42b746289c720
Saad Meddiche <s.meddiche@itroadgroup.com>
2024-12-02 15:04:28 +0100

    create findAllEmailsWithLimit

--- src/main/java/com/saadmeddiche/creditmanagement/repositories/PersonRepository.java
+++ src/main/java/com/saadmeddiche/creditmanagement/repositories/PersonRepository.java
@@ -3,6 +3,8 @@ package com.saadmeddiche.creditmanagement.repositories;
 import com.saadmeddiche.creditmanagement.entities.Person;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +16,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @EntityGraph(attributePaths = {"credits", "phoneNumbers"})
     List<Person> findAll();
 
+    @Query("SELECT p.email FROM Person p ORDER BY p.id ASC LIMIT :limit")
+    List<String> findAllEmailsWithLimit(@Param("limit") Long limit);
 
 }
