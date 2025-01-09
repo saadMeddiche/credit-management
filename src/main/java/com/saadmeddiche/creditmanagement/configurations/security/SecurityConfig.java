20ac0b1c8ce030d205f4c179c6f93ca0a617eaaf
Saad Meddiche <s.meddiche@itroadgroup.com>
2025-01-09 10:44:51 +0100

    SecurityConfig: repackage

--- src/main/java/com/saadmeddiche/creditmanagement/configurations/security/SecurityConfig.java
+++ src/main/java/com/saadmeddiche/creditmanagement/configurations/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.saadmeddiche.creditmanagement.security;
+package com.saadmeddiche.creditmanagement.configurations.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
