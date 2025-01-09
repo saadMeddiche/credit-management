232ff9b1751545168c9fd517eb4da418a4882c41
Saad Meddiche <s.meddiche@itroadgroup.com>
2025-01-09 09:32:26 +0100

    CustomStaticResourceProperties: update setter and add url and path as fields

--- src/main/java/com/saadmeddiche/creditmanagement/properties/CustomStaticResourceProperties.java
+++ src/main/java/com/saadmeddiche/creditmanagement/properties/CustomStaticResourceProperties.java
@@ -1,16 +1,36 @@
 package com.saadmeddiche.creditmanagement.properties;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter @Setter
+import java.nio.file.Path;
+
+@Getter
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "custom-static-resource")
 public class CustomStaticResourceProperties {
 
     private String location;
+
+    private String url;
+
+    private Path path;
+
+    public void setLocation(String location) {
+        this.location = replacePathSeparator(location);
+        this.path = Path.of(location);
+    }
+
+    public void setUrl(String url) {
+        this.url = replacePathSeparator(url);
+    }
+
+    // Remove '/' or '\' from the path
+    private String replacePathSeparator(String path) {
+        return path.replace("/", "").replace("\\", "");
+    }
+
 }
